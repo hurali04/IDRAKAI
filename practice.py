eafa69cc67c63e57fe0989c6511b6de5c320f6aa
Hur Ali <hurali498@gmail.com>
2025-07-28 17:32:03 +0500

    Added Task 4

--- practice.py
+++ practice.py
@@ -1,2 +1,25 @@
-a=int(input("Enter the value: "))
-print("The value is: " , a)
+import time
+
+def execution_time(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        print(f"Execution Time of '{func.__name__}': {end_time - start_time:.4f} seconds")
+        return result
+    return wrapper
+def log_calls(func):
+    def wrapper(*args, **kwargs):
+        print(f"Calling function: {func.__name__}")
+        print(f"Arguments: args={args}, kwargs={kwargs}")
+        result = func(*args, **kwargs)
+        print(f"Return value: {result}")
+        return result
+    return wrapper
+@log_calls
+@execution_time
+def multiply(a, b):
+    time.sleep(1)  # simulate delay
+    return a * b
+
+multiply(5, 10)
